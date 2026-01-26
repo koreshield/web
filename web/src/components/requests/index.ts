873b6363775a9f235009b869ee0649400bbb9734
1cbyc <isaacnsisong@gmail.com>
2026-01-26 20:35:37 +0400

    removed the frontend

--- web/src/components/requests/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./types";
-export * from "./utils";
-export * from "./request-inspector-drawer";
-export * from "./replay-modal";
-export * from "./request-tab-content";
-export * from "./response-tab-content";
-export * from "./full-capture-disabled-content";
