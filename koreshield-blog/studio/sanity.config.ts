06050832f08291cac93d6d7b7a36119261c3d0ea
Isaac · full stack <35623334+1cbyc@users.noreply.github.com>
2026-02-28 08:37:47 +0100

    feat(blog): migrate to sanity headless cms

---
+++ koreshield-blog/studio/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'koreshield-blog',
+
+  projectId: 'rdas6fhs',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
