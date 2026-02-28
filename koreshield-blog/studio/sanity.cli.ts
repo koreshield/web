ee46e1f32f0412575f2119b86bf468befc77ba86
Isaac · full stack <35623334+1cbyc@users.noreply.github.com>
2026-02-28 22:16:09 +0100

    chore(blog): remove hardcoded sanity project ids and datasets (#132)

--- koreshield-blog/studio/sanity.cli.ts
+++ koreshield-blog/studio/sanity.cli.ts
@@ -1,15 +1,19 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
-  api: {
-    projectId: 'rdas6fhs',
-    dataset: 'production'
-  },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
-  }
+	api: {
+		projectId: process.env.SANITY_STUDIO_PROJECT_ID as string,
+		dataset: process.env.SANITY_STUDIO_DATASET as string
+	},
+	deployment: {
+		/**
+		 * Enable auto-updates for studios.
+		 * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
+		 */
+		autoUpdates: true,
+	},
+	vite: (config) => ({
+		...config,
+		base: '/studio/',
+	}),
 })
