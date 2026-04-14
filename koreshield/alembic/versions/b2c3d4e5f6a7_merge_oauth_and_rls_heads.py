a0ed3a7054d8e02209e9036b376344ef231558e4
Isaac Nsisong <isaacnsisong@gmail.com>
2026-04-14 23:12:18 +0100

    fix(migrations): merge oauth and rls heads into single head

---
+++ koreshield/alembic/versions/b2c3d4e5f6a7_merge_oauth_and_rls_heads.py
@@ -0,0 +1,19 @@
+"""merge oauth and rls heads
+
+Revision ID: b2c3d4e5f6a7
+Revises: a1b2c3d4e5f6, a8b9c0d1e2f3
+Create Date: 2026-04-14 01:00:00.000000
+"""
+
+revision = "b2c3d4e5f6a7"
+down_revision = ("a1b2c3d4e5f6", "a8b9c0d1e2f3")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
